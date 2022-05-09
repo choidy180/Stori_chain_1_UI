c8dd07eb4e06b64f87d8dbb08086c3334bf05f6b
choidy180 <choidy180@naver.com>
2022-05-10 01:03:16 +0900

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import { GlobalStyle } from "../styles/global-style";
 import { theme } from '../styles/theme';
 import { useEffect, useState } from 'react';
 import { app, authService } from "../firebase/firebaseConfig";
-import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -22,16 +21,16 @@ function MyApp({ Component, pageProps }) {
     });
   }, []);
   return (
-      <RecoilRoot>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>감도 높은 취향 셀렉트몰 29CM | Next</title>
-        </Head>
-        <GlobalStyle/>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps}/>
-        </ThemeProvider>
-      </RecoilRoot>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>감도 높은 취향 셀렉트몰 29CM | Next</title>
+      </Head>
+      <GlobalStyle/>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps}/>
+      </ThemeProvider>
+    </>
   )
 }
 
